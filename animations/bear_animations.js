7ff84a0e70c0726b70b07283cd74d688689c42d1
semaurer <semaurer14@gmail.com>
2016-12-23 17:16:06 -0500

    Implement 4 bear sprites

---
+++ animations/bear_animations.js
@@ -0,0 +1,37 @@
+const bearImage = new Image();
+bearImage.src = "images/bear_sprites.png";
+
+function BearAnimations() {
+  return {
+    bearUp1: {
+      width: 40,
+      height: 40,
+      image: bearImage,
+      startX: 5,
+      startY: 125
+    },
+    bearLeft1: {
+      width: 42,
+      height: 40,
+      image: bearImage,
+      startX: 0,
+      startY: 51
+    },
+    bearRight1: {
+      width: 42,
+      height: 34,
+      image: bearImage,
+      startX: 0,
+      startY: 93
+    },
+    bearDown1: {
+      width: 42,
+      height: 42,
+      image: bearImage,
+      startX: 6,
+      startY: 0
+    }
+  };
+}
+
+module.exports = BearAnimations;

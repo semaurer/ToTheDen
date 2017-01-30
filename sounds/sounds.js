8b5dbe5c838eea1326542dd8969a920e5d3c3fee
semaurer <semaurer14@gmail.com>
2017-01-30 11:49:00 -0500

    Move explosion and growl sound logic into sounds class

--- sounds/sounds.js
+++ sounds/sounds.js
@@ -1,1 +1,36 @@
 const Sound = require("./sound.js");
+
+class sounds {
+  constructor() {
+    this.bearGrowl = new Sound();
+    this.bearGrowl.createSound("victory_growl");
+    this.explosionSound = new Sound();
+    this.explosionSound.createSound("death_explosion");
+    this.setupAudioToggle();
+    this.muted = false;
+  }
+
+  setupAudioToggle() {
+    document.getElementsByClassName('audio-toggle')[0]
+      .addEventListener("click", this.toggleAudio.bind(this));
+  }
+
+  toggleAudio() {
+    let audioButton = document.getElementsByClassName('audio-toggle')[0];
+    if (this.muted) {
+      this.BGAudio.volume = 0.2;
+      this.bearGrowl.volume = 0.1;
+      this.explosionSound.volume = 0.1;
+      this.muted = false;
+      audioButton.innerHTML = "Mute Audio";
+    } else {
+      this.BGAudio.volume = 0;
+      this.bearGrowl.sound.volume = 0;
+      this.explosionSound.sound.volume = 0;
+      this.muted = true;
+      audioButton.innerHTML = "Play Audio";
+    }
+  }
+}
+
+module.exports = sounds;
